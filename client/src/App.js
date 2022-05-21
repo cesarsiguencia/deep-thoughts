745b87341d231de9d7a2e06f4b349c3db594014b
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-21 17:59:19 -0400

    /

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'; //for React to use the Apollo
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import React from 'react';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 //this creates the link to graphql server with createHttplink, 
 // the server is running in the next localhost +1 for production, but when the app is done, change the uri to '/graphql', uri = uniform resource identifier
@@ -24,15 +31,43 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/profile/:username" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
+
 }
 
 export default App;
